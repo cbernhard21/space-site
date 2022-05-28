cbabd69d18c7c8c9863138a76f447d48f087bf3d
Christopher Bernhard <chris_bernhard@yahoo.com>
2022-05-28 20:54:28 +0000

    fixed adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter({
+// 			edge: false,
+// 			split: false
+// 		})
+// 	}
+// };
+
+// export default config;
+
+export default {
 	kit: {
-		adapter: adapter({
-			edge: false,
-			split: false
-		})
+	  adapter: adapter()
 	}
-};
-
-export default config;
+  };
